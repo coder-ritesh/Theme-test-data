4773588c239d66b302a16357a6f9897c1885e0bf
coder-ritesh <146415772+coder-ritesh@users.noreply.github.com>
2023-10-18 09:55:22 +0530

    Update global.js

--- assets/global.js
+++ assets/global.js
@@ -544,32 +544,32 @@ class ModalOpener extends HTMLElement {
 }
 customElements.define('modal-opener', ModalOpener);
 
-// class DeferredMedia extends HTMLElement {
-//   constructor() {
-//     super();
-//     const poster = this.querySelector('[id^="Deferred-Poster-"]');
-//     if (!poster) return;
-//     poster.addEventListener('click', this.loadContent.bind(this));
-//   }
+class DeferredMedia extends HTMLElement {
+  constructor() {
+    super();
+    const poster = this.querySelector('[id^="Deferred-Poster-"]');
+    poster.addEventListener('click', this.loadContent.bind(this));
+    this.loadContent(false);
+  }
 
-//   loadContent(focus = true) {
-//     window.pauseAllMedia();
-//     if (!this.getAttribute('loaded')) {
-//       const content = document.createElement('div');
-//       content.appendChild(this.querySelector('template').content.firstElementChild.cloneNode(true));
+  loadContent(focus = true) {
+    window.pauseAllMedia();
+    if (!this.getAttribute('loaded')) {
+      const content = document.createElement('div');
+      content.appendChild(this.querySelector('template').content.firstElementChild.cloneNode(true));
 
-//       this.setAttribute('loaded', true);
-//       const deferredElement = this.appendChild(content.querySelector('video, model-viewer, iframe'));
-//       if (focus) deferredElement.focus();
-//       if (deferredElement.nodeName == 'VIDEO' && deferredElement.getAttribute('autoplay')) {
-//         // force autoplay for safari
-//         deferredElement.play();
-//       }
-//     }
-//   }
-// }
+      this.setAttribute('loaded', true);
+      const deferredElement = this.appendChild(content.querySelector('video, model-viewer, iframe'));
+      if (focus) deferredElement.focus();
+      if (deferredElement.nodeName == 'VIDEO' && deferredElement.getAttribute('autoplay')) {
+        // force autoplay for safari
+        deferredElement.play();
+      }
+    }
+  }
+}
 
-// customElements.define('deferred-media', DeferredMedia);
+customElements.define('deferred-media', DeferredMedia);
 
 class SliderComponent extends HTMLElement {
   constructor() {
